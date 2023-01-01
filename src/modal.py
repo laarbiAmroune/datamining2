aeb84a03f41e99a3d036c2e6385f4b01536a71de
[Faycel ahcene] <ff_ahcene@esi.dz>
2023-01-01 16:40:55 +0100

    initial commit

---
+++ src/modal.py
@@ -0,0 +1,16 @@
+class DTree :
+    def __init__(self, maxDepth: int = 100):
+        pass
+
+    def fit(data):
+        pass
+
+    def predict(samples):
+        pass
+    
+    def predict_sample(sample):
+        pass
+    def gini(data):
+        pass
+
+    
